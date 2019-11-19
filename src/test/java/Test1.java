b672088c86912c89641425bcfddc44040a9596ef
wcj <only_angle@live.com>
2019-11-19 16:39:42 +0800

    first commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,12 @@
+/**
+ * User: zyyy
+ * Date: 2017/9/9
+ * Time: 11:25
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        MD5 md5 = new MD5();
+        System.out.println(md5.getMD5ofStr("123456789012345"));
+    }
+}
